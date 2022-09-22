f20768c9549fddd6c571d36ae149885cc927b14d
Denis <dfazylov@mail.ru>
2022-09-22 17:19:28 +0500

    Example005_ContitionalIfElse

---
+++ Example005_ContitionalIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "kseni")
+{
+    Console.WriteLine("Ура это же KSENI!!!");    
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
